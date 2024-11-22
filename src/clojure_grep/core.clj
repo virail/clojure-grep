06bacbc489e6ed35620d0bb456c8c95092431d5e
virail <xjbviralx@gmail.com>
2024-11-22 16:16:33 +0000

    Followed similar logic to clojure-spellchecker, read in file, split lines, then use regex to match on each line to see if pattern can be found

--- src/clojure_grep/core.clj
+++ src/clojure_grep/core.clj
@@ -1,7 +1,31 @@
 (ns clojure-grep.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
+(defn read-file
+  [path]
+  (set (clojure.string/split-lines (slurp path))))
+
+;; (defn found-in-file?
+;;   [set pattern]
+;;   (some #(re-find pattern %) set))
+
+(defn found-in-line?
+  [line pattern]
+  (re-find pattern line))
+
+(defn found-in-file?
+  [set pattern]
+  (if (empty? set)
+    nil 
+    (let [curr (first set) 
+          remaining (rest set)] 
+      (if (found-in-line? curr pattern) 
+        (first set) 
+        (recur remaining pattern)))))
+
+(defn -main 
   [& args]
-  (println "Hello, World!"))
+  (let [line (found-in-file? (read-file (first args)) (re-pattern (second args)))]
+    (if line
+      (println line)
+      (println "Pattern not found"))))
